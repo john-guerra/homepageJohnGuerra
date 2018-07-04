c5ebe8aa36a2c4ad5508c56cb17be9cfcd519298
John Alexis Guerra Gómez <john.guerra@gmail.com>
2018-07-04 11:14:58 -0500

    Adding new projects

--- img/projs/resize_gifs.sh
+++ img/projs/resize_gifs.sh
@@ -1,21 +1,21 @@
 # bash
 
 for filename in ./*.gif; do
-  if [[ $filename != *"_small"* ]]; then
+  if [[ $filename != *"_small"* ]] && [ ! -f "${filename%.gif}_small.gif" ]; then
     echo $filename
     convert $filename -coalesce -scale 120 -layers Optimize  "${filename%.gif}_small.gif"
   fi
 done
 
 for filename in ./*.png; do
-  if [[ $filename != *"_small"* ]]; then
+  if [[ $filename != *"_small"* ]] && [ ! -f "${filename%.png}_small.png" ]; then
     echo $filename
     convert $filename -scale 120   "${filename%.png}_small.png"
   fi
 done
 
 for filename in ./*.jpg; do
-  if [[ $filename != *"_small"* ]]; then
+  if [[ $filename != *"_small"* ]] && [ ! -f  "${filename%.jpg}_small.jpg" ]; then
     echo $filename
     convert $filename -scale 120   "${filename%.jpg}_small.jpg"
   fi
