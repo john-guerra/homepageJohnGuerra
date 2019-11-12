98d7203e09f12bb3f304cc3aee0a685086760fb3
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-11-12 10:54:06 -0500

    Adding new project images

--- js/papers.js
+++ js/papers.js
@@ -4,70 +4,122 @@ container.html("");
 var timeFmt = d3.timeParse("%m/%d/%Y");
 var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-d3.json("papers.json", function (err, papers) {
+d3.json("papers.json", function(err, papers) {
   if (err) throw err;
 
-  papers.forEach(function (d) {
+  papers.forEach(function(d) {
     d.date = timeFmt(d.date);
-    d.authors = d.authors.replace("John Alexis Guerra-Gomez", "<strong>John Alexis Guerra-Gomez</strong>");
+    d.authors = d.authors.replace(
+      "John Alexis Guerra-Gomez",
+      "<strong>John Alexis Guerra-Gomez</strong>"
+    );
   });
-  var hideCategories=["Presentation", "Tech Report","Software", "Trademark"];
-  var filteredPapers = papers.filter(function (d) { return hideCategories.indexOf(d.type)===-1; });
-  var sortByDate = function(a, b) { return d3.descending(a.date, b.date); };
-  var nestedPapers = d3.nest()
-    .key(function(d) { return d.category; })
-    .sortValues(sortByDate)
-    .entries(filteredPapers.sort(sortByDate));
 
-  container.append("h2")
+  var hideCategories = [
+    "Presentation",
+    "Tech Report",
+    "Software",
+    "Trademark",
+    "Panel"
+  ];
+  const categoriesOrder = {
+    Visualization: 0,
+    "Tree Visualization": 1,
+    "Photo Visualization": 2,
+    "Network Visualization": 3,
+    "Time Visualization": 6,
+    Accessibility: 5,
+    Other: 4
+  };
+
+  var filteredPapers = papers.filter(function(d) {
+    return hideCategories.indexOf(d.type) === -1;
+  });
+
+  var sortByDate = function(a, b) {
+    return d3.descending(a.date, b.date);
+  };
+
+  var nestedPapers = d3
+    .nest()
+    .key(function(d) {
+      return d.category;
+    })
+    .entries(filteredPapers.sort(sortByDate))
+    .sort((a, b) =>
+      d3.ascending(categoriesOrder[a.key], categoriesOrder[b.key])
+    );
+
+  container
+    .append("h2")
     .text("Publications" + " (" + filteredPapers.length + ")");
-  var papersSel = container.selectAll(".category")
-    .data(nestedPapers);
 
-  var catSel = papersSel.enter()
+  const row = container.append("div").attr("class", "row");
+  var papersSel = row.selectAll(".category").data(nestedPapers);
+
+  var catSel = papersSel
+    .enter()
     .append("div")
-    .attr("class", "category")
-    .style("page-break-before", "always");
+    .attr("class", "category col-md-6");
+  // .style("page-break-before", "always");
 
-  catSel.append("h3")
-    .text(function (d) { return d.key + " (" + d.values.length + ")"; });
+  catSel.append("h3").text(function(d) {
+    return d.key + " (" + d.values.length + ")";
+  });
 
   var paperSel = catSel
     .selectAll(".paper")
-    .data(function (d) { return d.values; })
+    .data(function(d) {
+      return d.values;
+    })
     .enter()
     .append("div")
     .attr("class", "paper");
 
-  paperSel.append("div")
+  paperSel
+    .append("div")
     .attr("class", "year")
-    .text(function (d) { return d.year; });
+    .text(function(d) {
+      return d.year;
+    });
 
-  var paperContentSel = paperSel.append("div")
-    .attr("class", "paper-content");
+  var paperContentSel = paperSel.append("div").attr("class", "paper-content");
 
-  paperSel.append("div")
-    .attr("class", "clearer");
+  paperSel.append("div").attr("class", "clearer");
 
-
-  paperContentSel.append("a")
+  paperContentSel
+    .append("a")
     .attr("class", "title")
-    .attr("href", function (d) { return d.link; })
-    .text(function (d) { return d.title; });
+    .attr("href", function(d) {
+      return d.link;
+    })
+    .text(function(d) {
+      return d.title;
+    });
 
-  paperContentSel.append("div")
+  paperContentSel
+    .append("div")
     .attr("class", "authors")
-    .html(function (d) { return d.authors; });
+    .html(function(d) {
+      return d.authors;
+    });
 
-  paperContentSel.append("div")
+  paperContentSel
+    .append("div")
     .attr("class", "venue")
-    .text(function (d) { return d.venue; });
+    .text(function(d) {
+      return d.venue;
+    });
 
-  paperContentSel.append("div")
+  paperContentSel
+    .append("div")
     .attr("class", "type")
-    .style("color", function (d) { return color(d.type);})
-    .text(function (d) { return d.type; });
+    .style("color", function(d) {
+      return color(d.type);
+    })
+    .text(function(d) {
+      return d.type;
+    });
 
-  catSel.append("div")
-    .attr("class", "clearer");
+  catSel.append("div").attr("class", "clearer");
 });
--- js/projectsComb.js
+++ js/projectsComb.js
@@ -3,68 +3,69 @@
   /* global d3, CombColumns */
   //Function to call when you mouseover a node
   function mover(d) {
-    var el = d3.select(this)
+    var el = d3
+      .select(this)
       .transition()
       .duration(10)
-      .style("opacity", 0.3)
-      ;
+      .style("opacity", 0.3);
   }
 
   //Mouseout function
   function mout(d) {
-    var el = d3.select(this)
-       .transition()
-       .duration(1000)
-       .style("opacity", 1)
-       ;
+    var el = d3
+      .select(this)
+      .transition()
+      .duration(1000)
+      .style("opacity", 1);
   }
 
   //svg sizes and margins
   var margin = {
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0
   };
 
   var width = 850;
   var height = 2050;
 
   //The number of columns and rows of the heatmap
-  var MapColumns = window.CombColumns || 4,
+  var MapColumns = window.CombColumns || 5,
     MapRows; // I'll compute this later
-
 
   //Set the hexagon radius
   var hexbin = d3.hexbin();
 
   //Create SVG element
-  d3.select("#projectsComb").append("svg")
-      // .style("width", "100%")
-      // .style("height", "100%")
-      // .attr("width", width + margin.left + margin.right)
-      // .attr("height", height + margin.top + margin.bottom)
+  d3.select("#projectsComb")
+    .append("svg")
+    // .style("width", "100%")
+    // .style("height", "100%")
+    // .attr("width", width + margin.left + margin.right)
+    // .attr("height", height + margin.top + margin.bottom)
     .append("defs")
     .append("clipPath")
-    .attr("id","hexClip")
+    .attr("id", "hexClip")
     .append("path");
 
-  var svg = d3.select("#projectsComb>svg")
+  var svg = d3
+    .select("#projectsComb>svg")
     .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var projects = svg.append("g")
-    .attr("class", "projects");
-  var mesh = svg.append("path")
-    .attr("class", "mesh");
+  var projects = svg.append("g").attr("class", "projects");
+  var mesh = svg.append("path").attr("class", "mesh");
 
   function updateComb(data) {
     // updateComb radius
     var svgNode = d3.select("#projectsComb").node();
     width = svgNode ? svgNode.clientWidth : width;
-    height = d3.select("#achievementsText").node() ? d3.select("#achievementsText").node().clientHeight: window.innerHeight;
-    var hexRadius = width/((MapColumns + 0.5) * Math.sqrt(3));
-    MapRows = Math.floor((height)/(1.5*hexRadius))+1;
+    height = d3.select("#achievementsText").node()
+      ? d3.select("#achievementsText").node().clientHeight
+      : window.innerHeight;
+    var hexRadius = width / ((MapColumns + 0.5) * Math.sqrt(3));
+    MapRows = Math.floor(height / (1.5 * hexRadius)) + 1;
     //Set the new height and width of the SVG based on the max possible
     // width = MapColumns*hexRadius*Math.sqrt(3);
     // heigth = MapRows*1.5*hexRadius+0.5*hexRadius;
@@ -73,40 +74,49 @@
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom);
 
-
     var centers = hexbin
       .radius(hexRadius)
       .size([width, height])
       .centers();
 
     d3.select("#projectsComb > svg #hexClip > path")
-      .attr("transform", "translate(" + (Math.sin(Math.PI/3) * hexRadius) + "," + hexRadius+")")
-      .attr("d", function () { return hexbin.hexagon(); });
+      .attr(
+        "transform",
+        "translate(" + Math.sin(Math.PI / 3) * hexRadius + "," + hexRadius + ")"
+      )
+      .attr("d", function() {
+        return hexbin.hexagon();
+      });
 
-    var fillingI = 0, importantI = data.length-1; // Two indexes to fill the comb
+    var fillingI = 0,
+      importantI = data.length - 1; // Two indexes to fill the comb
     // Do smart assigment, don't put a project on a cut hexagon
-    centers.forEach(function (center, i) {
+    centers.forEach(function(center, i) {
       center.j = Math.round(center[1] / (hexRadius * 1.5));
-      center.i = Math.round((center[0] - (center.j & 1) * hexRadius * Math.sin(Math.PI / 3)) / (hexRadius * 2 * Math.sin(Math.PI / 3)));
-      center.x = center[0] - Math.sin(Math.PI/3) * hexRadius;
+      center.i = Math.round(
+        (center[0] - (center.j & 1) * hexRadius * Math.sin(Math.PI / 3)) /
+          (hexRadius * 2 * Math.sin(Math.PI / 3))
+      );
+      center.x = center[0] - Math.sin(Math.PI / 3) * hexRadius;
       center.y = center[1] - hexRadius;
 
-      var even = center.j%2===0;
-      var proj ;
-      if ( (even && center.i=== 0) ||
-        (!even && center.i>=MapColumns) ||
-        center.j===0 ||
-        center.j===MapRows-1) {
+      var even = center.j % 2 === 0;
+      var proj;
+      if (
+        (even && center.i === 0) ||
+        (!even && center.i >= MapColumns) ||
+        center.j === 0 ||
+        center.j === MapRows - 1
+      ) {
         // Unimportant projects here
-        proj = data[fillingI++%data.length];
+        proj = data[fillingI++ % data.length];
       } else {
         proj = data[importantI--];
-        if (importantI===0) {
-          importantI = data.length-1;
+        if (importantI === 0) {
+          importantI = data.length - 1;
         }
       }
-      if (proj)
-        proj.index = i;
+      if (proj) proj.index = i;
       center.proj = proj;
     });
 
@@ -120,32 +130,36 @@
     //   proj.index = i;
     // });
 
-
-    var projectsSel = projects.selectAll(".projectHex")
-      .data(centers);
-    projectsSel.enter().append("image")
+    var projectsSel = projects.selectAll(".projectHex").data(centers);
+    projectsSel
+      .enter()
+      .append("image")
       .attr("class", "projectHex")
       .merge(projectsSel)
-      .filter(function (d) { return d.proj; })
-      .attr("xlink:href", function (d) {
-        var splitName = d.proj.thumb.split(".");
-        return "img/projs/"+ splitName[0] + "_small." + splitName[1];
+      .filter(function(d) {
+        return d.proj;
       })
-      .attr("width", (hexRadius*2))
-      .attr("height", (hexRadius*2))
+      .attr("xlink:href", function(d) {
+        var splitName = d.proj.thumb.split(".");
+        return "img/projs/" + splitName[0] + "_small." + splitName[1];
+      })
+      .attr("width", hexRadius * 2)
+      .attr("height", hexRadius * 2)
       .attr("preserveAspectRatio", "xMidYMid slice")
       .attr("clip-path", "url(#hexClip)")
-      .on("click", function (d) {
+      .on("click", function(d) {
         window.open(d.proj.url, "_blank");
       })
 
-      .attr("transform", function (d) {
+      .attr("transform", function(d) {
         return "translate(" + d.x + "," + d.y + ")";
       })
       .on("mouseover", mover)
       .on("mouseout", mout)
       .append("title")
-      .text(function (d) { return d.proj.project; });
+      .text(function(d) {
+        return d.proj.project;
+      });
 
     projectsSel.exit().remove();
 
@@ -183,23 +197,29 @@
     //   .attr("height", 500)
     //   // .attr("clip-path", "url(#hexClip)")
 
-
-
-    d3.select(window)
-      .on("resize", function () { updateComb(data); });
-      // .each(function () { updateComb(data); });
-
+    d3.select(window).on("resize", function() {
+      updateComb(data);
+    });
+    // .each(function () { updateComb(data); });
   }
 
-  d3.csv("projects.csv", function (d) {
-    d.date= d3.timeParse("%m/%d/%Y")(d.date);
-    return d;
-  },function (err, data) {
-    if (err) throw err;
-    updateComb(data.sort(function (a, b) {
-      return d3.ascending(a.rating, b.rating)
-        || d3.ascending(a.date, b.date);
-    }));
-  });
+  const dateFmt = d3.timeParse("%m/%d/%Y");
 
+  d3.csv(
+    "projects.csv",
+    function(d) {
+      d.date = dateFmt(d.date);
+      return d;
+    },
+    function(err, data) {
+      if (err) throw err;
+      updateComb(
+        data.sort(function(a, b) {
+          return (
+            d3.ascending(a.rating, b.rating) || d3.ascending(a.date, b.date)
+          );
+        })
+      );
+    }
+  );
 })();
