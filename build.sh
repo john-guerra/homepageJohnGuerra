d1eb3d7e80738a342d2b0550337a4db7a314754b
John Alexis Guerra Gómez <john.guerra@gmail.com>
2023-09-30 17:30:52 -0700

    Adding partials

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # for each ejs file in the src folder compile it to html and save it in the root folder
-for file in ./src/*.ejs; do
-  ejs $file -o ./$(basename $file .ejs).html && prettier --write ./$(basename $file .ejs).html
+for file in ./src/*.ejs.html; do
+  ejs $file -o ./$(basename $file .ejs.html).html && prettier --write ./$(basename $file .ejs.html).html
 done
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-rsync -avzgu --delete --exclude=node_modules --exclude=.git --partial -e "ssh -i /Users/aguerra/Dropbox/dutoVizNew.pem" * ubuntu@johnguerra.co:/var/www/johnguerra.co
+rsync -avzgu --delete --exclude=src --exclude=node_modules --exclude=.git --partial -e "ssh -i /Users/aguerra/Dropbox/dutoVizNew.pem" * ubuntu@johnguerra.co:/var/www/johnguerra.co
 
 # rsync -avzgu --delete --partial -e "ssh -i /Users/aguerra/Dropbox/tweetometro2.pem" * ubuntu@18.231.179.33:/var/www/johnguerra.co
 # rsync -avzgu --delete --partial -e "ssh -i /Users/aguerra/Dropbox/tweetometro_paz.pem" * ubuntu@34.230.24.73:/var/www/johnguerra.co
